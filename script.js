644a3b60cc6691b385f57877ea3e140ee795a572
FIXED-TERM Joshi Pushpesh (MS/ECJ2-PS) <huo3kor@bosch.com>
2024-04-04 20:05:12 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const numberBtns=document.querySelectorAll('.number');
+const operatorBtns=document.querySelectorAll('.operator');
+
+const lowerScreen=document.querySelector('.lowerScreen');
+const upperScreen=document.querySelector('.lowerScreen');
+
+numberBtns.forEach(function(btn){
+    btn.addEventListener('click',(e)=>{
+        let num=e.target.id;
+        
+    })
+})
