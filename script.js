07edd0a6a86dceb8d2ba277ad71dcce4b3ce1a70
FIXED-TERM Joshi Pushpesh (MS/ECJ2-PS) <huo3kor@bosch.com>
2024-04-05 11:52:19 +0530

    added js functions

--- script.js
+++ script.js
@@ -6,9 +6,47 @@ const operatorBtns=document.querySelectorAll('.operator');
 const lowerScreen=document.querySelector('.lowerScreen');
 const upperScreen=document.querySelector('.lowerScreen');
 
-numberBtns.forEach(function(btn){
-    btn.addEventListener('click',(e)=>{
-        let num=e.target.id;
-        
-    })
-})
+let firstNum='';
+let secondNum='';
+let operator='';
+
+
+
+function operate(firstNum,secondNum,operator){
+    let result;
+    switch(operator){
+        case add:
+            result=add(firstNum,secondNum);
+            break;
+        case sub:
+            result=sub(firstNum,secondNum);
+            break;
+        case mul:
+            result=mul(firstNum,secondNum);
+            break;
+        case div:
+            result=div(firstNum,secondNum);
+            break;            
+    }
+    return result;
+}
+
+
+function add(firstNum,secondNum){
+    return firstNum+secondNum;
+}
+
+function sub(firstNum,secondNum){
+    return firstNum-secondNum;
+}
+
+function div(firstNum,secondNum){
+    if(firstNum===0)
+        return ('Not Defined');
+    else
+        return firstNum/secondNum;
+}
+
+function mul(firstNum,secondNum){
+    return firstNum*secondNum;
+}
